161b5e75a9ba5399a25bed717f0045f244684085
ferdousalam007 <iamferdousalam007@gmail.com>
2021-08-18 12:03:41 +0600

    first step complete

--- ruff.js
+++ ruff.js
@@ -1,3 +1,4 @@
+// 01 First step
 document.getElementById('case-plus').addEventListener('click',function(){
     const caseInput = document.getElementById('case-number');
     const caseNumber = caseInput.value;
