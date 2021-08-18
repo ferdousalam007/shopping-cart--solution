d47e3d43402d6784ac8ee8e5f96a7b9b8207c9b8
ferdousalam007 <iamferdousalam007@gmail.com>
2021-08-18 15:18:45 +0600

    all update product price

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
+function updateNumber(product,price,isIncreasing){
+    const productInput = document.getElementById(product +'-number');
+    let productNumber = productInput.value;
+    
+    if(isIncreasing == true){
+        productNumber = parseInt(productNumber) + 1;
+    }
+    else if(productNumber > 0){
+        productNumber = parseInt(productNumber) -1;
+    }
+    productInput.value =productNumber;
+ //update case total
+  const productTotal = document.getElementById(product + '-total');
+  productTotal.innerText = productNumber * price;
+
+}
+//phone increse and decrease event
+document.getElementById('phone-plus').addEventListener('click',function(){
+    updateNumber('phone', 1219, true);
+})
+document.getElementById('phone-minus').addEventListener('click',function(){
+    updateNumber('phone', 1219, false);
+})
+
+
+//case increse and decrease event
 document.getElementById('case-plus').addEventListener('click',function(){
-    const caseInput = document.getElementById('case-number');
-    const caseNumber = caseInput.value;
-    caseInput.value = parseInt(caseNumber) + 1;
-    console.log(caseNumber);
+    updateNumber('case', 59, true);
+
+})
+document.getElementById('case-minus').addEventListener('click',function(){
+    updateNumber('case', 59, false);
 })
--- ruff.js
+++ ruff.js
@@ -5,3 +5,37 @@ document.getElementById('case-plus').addEventListener('click',function(){
     caseInput.value = parseInt(caseNumber) + 1;
     console.log(caseNumber);
 })
+document.getElementById('case-minus').addEventListener('click',function(){
+    const caseInput = document.getElementById('case-number');
+    const caseNumber = caseInput.value;
+    caseInput.value = parseInt(caseNumber) -1;
+})
+
+
+
+// part two
+function updateNumber(isIncreasing){
+    const caseInput = document.getElementById('case-number');
+    let caseNumber = caseInput.value;
+    
+    if(isIncreasing == true){
+        caseNumber = parseInt(caseNumber) + 1;
+    }
+    else if(caseNumber > 0){
+        caseNumber = parseInt(caseNumber) -1;
+    }
+    caseInput.value =caseNumber;
+ //update case total
+  const caseTotal = document.getElementById('case-total');
+  caseTotal.innerText = caseNumber *59;
+
+}
+
+
+document.getElementById('case-plus').addEventListener('click',function(){
+    updateNumber(true);
+
+})
+document.getElementById('case-minus').addEventListener('click',function(){
+    updateNumber(false);
+})
